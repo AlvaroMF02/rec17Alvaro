83b0373321f6df0e04d218e85c45aeaca5f87aa6
alvaro <alvaromarflo@gmail.com>
2022-05-31 14:43:07 +0200

    ññ

--- src/main/java/ejercicio1/Programa.java
+++ src/main/java/ejercicio1/Programa.java
@@ -10,7 +10,9 @@ public class Programa {
     
     public static void main(String[] args) {
         //LECTURA DEL JSON
+        LecturaJSON listaViajes = new LecturaJSON();
         
+        listaViajes.listaViajes(lista);
     }
         
         
